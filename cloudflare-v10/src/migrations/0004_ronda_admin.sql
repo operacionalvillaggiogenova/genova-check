35ac804d9e365b93f22013bb433feeb07b43440c
operacionalvillaggiogenova <operacional.villaggiogenova@gmail.com>
2026-09-01 20:31:50 -0300

    Delete cloudflare-v10/src directory

--- cloudflare-v10/src/migrations/0004_ronda_admin.sql
+++
@@ -1,3 +0,0 @@
-PRAGMA foreign_keys = ON;
-CREATE INDEX IF NOT EXISTS idx_ronda_sessions_vigilante ON ronda_sessions(vigilante);
-CREATE INDEX IF NOT EXISTS idx_ronda_checkpoints_status ON ronda_checkpoints(session_id,status);
